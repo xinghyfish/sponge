bf35c6a9f4d6a4a58ac2e00b440dca0ba3bad406
xosmos <xinghy.fish@qq.com>
2023-02-23 02:16:25 +0800

    feat: complete code of `tcp_receiver.hh` and `tcp_receiver.cc`

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,26 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader &header = seg.header();
+    if (!this->_syn) {
+        if (!header.syn)
+            return;
+        this->_syn = true;
+        this->_isn = header.seqno;
+    }
+
+    uint64_t checkpoint = this->_reassembler.stream_out().bytes_written();
+    uint64_t index = unwrap(header.seqno, this->_isn, checkpoint) - 1 + (header.syn);
+    this->_reassembler.push_substring(seg.payload().copy(), index, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if (!this->_syn)
+        return std::nullopt;
+    uint64_t abs_ackno = this->_reassembler.stream_out().bytes_written() + 1 + this->_reassembler.stream_out().input_ended();
+    return wrap(abs_ackno, this->_isn);
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    return this->_capacity - this->_reassembler.stream_out().buffer_size();
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -14,18 +14,21 @@
 //! the acknowledgment number and window size to advertise back to the
 //! remote TCPSender.
 class TCPReceiver {
+private:
+    bool _syn;
+	WrappingInt32 _isn;
     //! Our data structure for re-assembling bytes.
     StreamReassembler _reassembler;
 
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
-  public:
+public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _syn(false), _isn(0), _reassembler(capacity), _capacity(capacity) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
